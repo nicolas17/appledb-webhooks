2bd8d11bf39caa54acc1151b3de2bfcab787eead
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2023-07-16 01:19:11 -0300

    Add test sending more realistic data in the request

--- tests.py
+++ tests.py
@@ -4,6 +4,7 @@
 
 import unittest
 import hmac, hashlib
+import json
 from werkzeug.test import Client
 
 import appledb_filter
@@ -24,17 +25,21 @@ class TestApp(unittest.TestCase):
         response = self.client.get("/webhook")
         self.assertEqual(response.status_code, 405)
 
-    def do_signed_post(self, data, key=b'12345678'):
+    def do_signed_post(self, data, key=b'12345678', headers=None, **kwargs):
         sig_sha1 = hmac.new(key, msg=data, digestmod=hashlib.sha1)
         sig_sha256 = hmac.new(key, msg=data, digestmod=hashlib.sha256)
+        rheaders = {
+            "content-type": "application/json",
+            "x-hub-signature":      f"sha1={sig_sha1.hexdigest()}",
+            "x-hub-signature-256":  f"sha256={sig_sha256.hexdigest()}"
+        }
+        if headers is not None:
+            rheaders.update(headers)
         return self.client.post(
             "/webhook",
             data=data,
-            headers={
-                "content-type": "application/json",
-                "x-hub-signature":      f"sha1={sig_sha1.hexdigest()}",
-                "x-hub-signature-256":  f"sha256={sig_sha256.hexdigest()}"
-            }
+            headers=rheaders,
+            **kwargs
         )
 
     def test_bad_signature(self):
@@ -51,6 +56,30 @@ class TestApp(unittest.TestCase):
         data = b'invalid'
         response = self.do_signed_post(data)
         self.assertEqual(response.status_code, 400)
+
+    def test_forwarding(self):
+        data = json.dumps({
+            "ref": "refs/heads/main",
+            "before": "b444ac06613fc8d63795be9ad0beaf55011936ac",
+            "after": "109f4b3c50d7b0df729d299bc6f8e9ef9066971f",
+            "repository": {
+                "id": 12345678,
+                "name": "appledb",
+                "full_name": "littlebyteorg/appledb",
+            },
+            "pusher": {
+                "name": "nicolas17",
+                "email": "nicolas17@example.com"
+            },
+            "sender": {
+                "login": "nicolas17",
+            },
+            "forced": False
+        }).encode('utf8')
+        response = self.do_signed_post(data, headers={
+            "X-GitHub-Event": "push"
+        })
+        self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
